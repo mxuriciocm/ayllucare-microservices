26d9bbfa23675307be8541db378bb29d4774d510
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2025-11-13 18:02:53 -0500

    feat: add microservice-iam module with initial setup and configurations

---
+++ microservice-iam/src/main/java/com/microservice/iam/MicroserviceIamApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceIamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceIamApplication.class, args);
+	}
+
+}
---
+++ microservice-iam/src/test/java/com/microservice/iam/MicroserviceIamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceIamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
