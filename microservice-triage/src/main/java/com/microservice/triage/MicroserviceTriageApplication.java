e7855075edcb3c54ea8070eb7e520ca6bd2ea1ef
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2025-11-14 13:18:24 -0500

    feat: add microservice-triage module with initial setup and configurations

---
+++ microservice-triage/src/main/java/com/microservice/triage/MicroserviceTriageApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.triage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTriageApplication.class, args);
+	}
+
+}
