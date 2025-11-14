1212fa006d62c856d4c49a5ff02bbabad7595bb8
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2025-11-13 21:30:28 -0500

    feat: add microservice-profiles module with initial setup and configurations

---
+++ microservice-profiles/src/main/java/com/microservice/profiles/MicroserviceProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.profiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceProfilesApplication.class, args);
+	}
+
+}
