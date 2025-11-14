c364f00376e8e54c8e6fec4ac365cedb19735ff2
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2025-11-14 00:29:37 -0500

    feat: add microservice-anamnesis module with initial setup and configurations

---
+++ microservice-anamnesis/src/main/java/com/microservice/anamnesis/MicroserviceAnamnesisApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.anamnesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAnamnesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAnamnesisApplication.class, args);
+	}
+
+}
