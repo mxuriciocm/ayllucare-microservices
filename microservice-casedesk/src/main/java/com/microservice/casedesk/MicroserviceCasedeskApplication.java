890fa6081b972ddcfa75d689f815d468f5238439
Mauricio Chacon <mauriciochaconmartinez05@gmail.com>
2025-11-16 10:35:35 -0500

    feat: add microservice-casedesk with initial setup, configurations, and properties

---
+++ microservice-casedesk/src/main/java/com/microservice/casedesk/MicroserviceCasedeskApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.casedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCasedeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCasedeskApplication.class, args);
+	}
+
+}
---
+++ microservice-casedesk/src/test/java/com/microservice/casedesk/MicroserviceCasedeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.casedesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCasedeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
